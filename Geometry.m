9a7bd6116cea582902b15fe92793632e6db4b0bf
Alex <Alexg91024@gmail.com>
2022-03-21 20:19:41 -0400

    created initial Geometry.m function file

---
+++ Geometry.m
@@ -0,0 +1,16 @@
+function [a, b, I] = Geometry(cross_section, area, orientation)
+% TODO: write function descriptions
+%UNTITLED2 Summary of this function goes here        
+%   Detailed explanation goes here
+
+end
+
+
+
+
+
+% Salvatore L Greco <slgreco@buffalo.edu>
+% Alexander M Gross <amgross@buffalo.edu>
+% Geometry.m
+% EAS230
+% Professor Sabato, Professor Ali
