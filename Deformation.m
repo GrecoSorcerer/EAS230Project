e432d7cce662f093eff91bb4c35258a39af4562b
Alex <Alexg91024@gmail.com>
2022-03-22 18:11:40 -0400

    started Deformation.m

---
+++ Deformation.m
@@ -0,0 +1,20 @@
+function [z] = Deformation(cross_section, area, orientation)
+% Computes the deformations by Az = b equivalence 
+% 
+% Inputs: 
+%         g - gravitational constant
+%         mu - mass per unit length of the beam
+%         E - young's modulus
+%         I - second moment of area of the beam
+%         dx - spacing between the points 
+%         f - M by 1 column vector representing the point load
+% Output: 
+%         z - M by 1 column vector of deformations 
+
+end
+
+% Salvatore L Greco <slgreco@buffalo.edu>
+% Alexander M Gross <amgross@buffalo.edu>
+% Deformation.m
+% EAS230
+% Professor Sabato, Professor Ali
