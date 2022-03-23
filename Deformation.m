dc075a5e2ce38b7dc5feebbdd0cd239d1d36e774
Salapadakey <slgreco@buffalo.edu>
2022-03-22 22:30:57 -0400

    Define solution vector, coeff matrix and solution output for Deformation.m, untested.

--- Deformation.m
+++ Deformation.m
@@ -1,4 +1,4 @@
-function [z] = Deformation(cross_section, area, orientation)
+function [z] = Deformation(g, mu, E, I, dx, f)
 % Computes the deformations by Az = b equivalence 
 % 
 % Inputs: 
@@ -10,6 +10,20 @@ function [z] = Deformation(cross_section, area, orientation)
 %         f - M by 1 column vector representing the point load
 % Output: 
 %         z - M by 1 column vector of deformations 
+
+    M    = 101;   % Design doc says to let M = 101.
+
+    % b vector
+    % Solution vector equations 7-11.
+    b = [[0 0], ones([M-4,1]).*( ( (-mu*g)-f ).*( (dx.^4)./(E*I) ) ) , [0 0]]';
+    
+    % set up the matrix to compute z, corresponds to the coefficients  
+    % of the equations 7-11.
+    A = [[1, zeros([1,M-1])]; [-3, 4, -1, zeros([1, M-3])]; ones([[M-4,M]]).*[1, -4, 6, -4, 1, zeros([M-5, 1])]; [zeros([1, M-3]), 1, -4, 3]; [zeros([1, M-1]), 1]];
+    
+    % Solve for z by left dividing the coefficient matrix A by the 
+    % solution vector v.
+    z = A\b;
 
 end
 
